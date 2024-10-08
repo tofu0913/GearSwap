611a84ba776a4dd5672b469664dbb654d74ae4fa
Cliff Chen <cfchen@futuredial.com>
2024-10-08 20:49:28 +0800

    Add brd and sch

---
+++ BRD.lua
@@ -0,0 +1,273 @@
+
+mylib = require('mylib')
+require('mylibs/caster')
+
+function get_sets()
+    set_language('japanese')
+    
+    dummy = false
+    mode = nil
+    
+    sets.instrument = {}
+    sets.instrument.horn = {
+        range="ギャッラルホルン",
+        ammo=empty,
+    }
+    sets.instrument.HonorMarch = {
+        range="マルシュアス",
+        ammo=empty,
+    }
+    sets.instrument.dummy = {
+        range="ダウルダヴラ",
+        ammo=empty,
+    }
+    
+    sets.fc = {
+        body="インヤガジュバ+2",
+        hands={ name="ＧＥゲージ+1", augments={'Phys. dmg. taken -4%','Magic dmg. taken -3%','Song spellcasting time -5%',}},
+        legs="アヤモコッシャレ+2",
+        feet="ナヴォンクラッコー",
+        waist="エンブラサッシュ",
+        left_ear="ロケイシャスピアス",
+        -- right_ear="エンチャンピアス+1",
+        left_ring="キシャールリング",
+        right_ring="プロリクスリング",
+        back={ name="インタラアスケープ", augments={'CHR+20','Mag. Acc+20 /Mag. Dmg.+20','CHR+10','"Fast Cast"+10',}},
+    }
+    sets.fc.song = set_combine(sets.fc, {
+        head="ＦＬキャロ+2",--"ＦＬキャロ+3",
+        feet={ name="テルキネピガッシュ", augments={'Song spellcasting time -7%',}},
+        -- neck="ボルトサージトルク",
+    })
+    sets.fc.Cure = set_combine(sets.fc, {
+        feet={ name="ヴァニヤクロッグ", augments={'"Cure" potency +5%','"Cure" spellcasting time -15%','"Conserve MP"+6',}},
+        waist="アサビクサッシュ+1",
+        right_ear="メンデカントピアス",
+    })
+    
+    sets.buff = {
+        main="カーリ",-- { name="カルンウェナン", augments={'Path: A',}},
+        -- sub="アムラピシールド",
+        body="ＦＬオングルリヌ+3",
+        hands="ＦＬマンシェト+3",
+        legs="インヤガシャルワ+2",
+        feet="ＢＲスリッパー+2",--"ＢＲスリッパー+3",
+        neck="月虹の呼び子+1",
+        right_ear={ name="フィリピアス+1", augments={'System: 1 ID: 1676 Val: 0','Accuracy+11','Mag. Acc.+11','Damage taken-3%',}},
+    }
+    sets.buff.Ballad = {
+        legs="ＦＬラングラヴ+1",--"ＦＬラングラヴ+3",
+    }
+    sets.buff.Scherzo = {
+        legs="フィリコテュルヌ",--"ＦＬコテュルヌ+2",
+    }
+    sets.buff.Paeon = {
+        -- head="Brioso Roundlet +2",
+    }
+    sets.buff.March = {
+        hands="ＦＬマンシェト+3",
+    }
+    sets.buff.Lullaby = {
+        -- range="ダウルダヴラ",
+        range="ギャッラルホルン",
+        -- hands="ＢＲカフス+3",
+    }
+    sets.macc = {
+        main="トーレット",
+    }
+
+    sets.idle = {
+        head="ニャメヘルム",--"ＦＬキャロ+3",
+        body="ニャメメイル",
+        hands="ＦＬマンシェト+3",
+        legs="ニャメフランチャ",
+        feet={ name="ニャメソルレット", augments={'Path: B',}},
+        neck="バーシチョーカー+1",
+        waist="プラチナモグベルト",
+        right_ring="シュネデックリング",
+    }
+    sets.engage = {
+        waist={ name="セールフィベルト+1", augments={'Path: A',}},
+        left_ear="テロスピアス",
+        right_ear="セサンスピアス",
+        left_ring="シーリチリング+1",
+        right_ring="イフラマドリング",
+    }
+
+    sets.ja = {}
+    sets.ja['Nightingale'] = {
+        -- feet={ name="ＢＩスリッパー+3", augments={'Enhances "Nightingale" effect',}},
+        feet={ name="ビフスリッパー", augments={'Enhances "Nightingale" effect',}},
+    }
+    sets.ja['Troubadour'] = {
+        body="ＢＩジュストコル+1",--"ＢＩジュストコル+3",
+        -- body={ name="ＢＩジュストコル+2", augments={'Enhances "Troubadour" effect',}},
+    }
+    sets.ja['Soul Voice'] = {
+        legs={ name="ビフキャニオンズ", augments={'Enhances "Soul Voice" effect',}},
+    }
+
+    sets.ws = {
+        body="ＢＩジュストコル+1",--"ＢＩジュストコル+3",
+        feet={ name="ニャメソルレット", augments={'Path: B',}},
+    }
+
+    sets.ma = {}
+    sets.ma.Cure = {
+        neck="ノデンズゴルゲット",
+        feet="ヴァニヤクロッグ",
+        left_ear="メンデカントピアス",
+        right_ear="カラミタスピアス",
+        left_ring="ナジの包帯",
+    }
+    sets.ma.Stoneskin = {
+        neck="ノデンズゴルゲット",
+        waist="ジーゲルサッシュ",
+    }
+    
+    sets.walk = {
+        right_ring="シュネデックリング",
+    }
+    sets.walk.adoulin = set_combine(sets.walk, {
+        body="カウンセラーガーブ",
+    })
+
+    send_command('input /macro book 1;')
+    send_command('wait 2;input /lockstyleset 4')
+end
+
+function precast(spell)
+    local set_equip = nil
+    
+    if spell.type == 'BardSong' then
+        if dummy then
+            set_equip = sets.instrument.dummy
+            dummy = false
+        elseif spell.english == 'Honor March' then
+            set_equip = sets.instrument.HonorMarch
+        else
+            if string.find(spell.english, 'Paeon') or mode == 'HARP' then
+                set_equip = sets.instrument.dummy
+            else
+                set_equip = sets.instrument.horn
+            end
+        end
+    
+        set_equip = set_combine(set_equip, sets.fc.song)
+        
+    elseif spell.action_type == 'Magic' then
+        if string.find(spell.english,'Cur') then
+            set_equip = sets.fc.Cure
+        else
+            set_equip = sets.fc
+        end
+        
+    elseif spell.english == 'Spectral Jig' and windower.ffxi.get_ability_recasts()[218] == 0 then
+        windower.ffxi.cancel_buff(71)
+    end
+
+    if set_equip then
+        equip(set_equip)
+    end
+end
+
+function midcast(spell)
+    local set_equip = nil
+
+    if spell.type == 'BardSong' then
+        set_equip = sets.buff
+        if string.find(spell.english, 'Ballad') then set_equip = set_combine(set_equip, sets.buff.Ballad)
+        elseif string.find(spell.english, 'Scherzo') then set_equip = set_combine(set_equip, sets.buff.Scherzo)
+        elseif string.find(spell.english, 'Paeon') then set_equip = set_combine(set_equip, sets.buff.Paeon)
+        elseif string.find(spell.english, 'March') then set_equip = set_combine(set_equip, sets.buff.March)
+        elseif string.find(spell.english, 'Lullaby') then 
+            set_equip = set_combine(set_equip, sets.buff.Lullaby)
+        end
+    elseif sets.ja[spell.english] then
+        set_equip = sets.ja[spell.english]
+        
+    elseif spell.type == 'WeaponSkill' then
+        set_equip = sets.ws
+    
+    elseif spell.action_type == 'Magic' then
+        if string.find(spell.english,'Cur') and spell.name ~= 'Cursna' then
+            set_equip = sets.ma.Cure
+        elseif sets.ma[spell.english] then
+            set_equip = sets.ma[spell.english]
+        end
+    end
+    
+    if set_equip then
+        equip(set_equip)
+    end
+end
+
+function aftercast(spell)
+    -- windower.add_to_chat(spell.english)
+    setIdle()
+end
+
+function status_change(new,old)
+
+    setIdle()
+end
+
+function setIdle()
+    local set_equip = sets.idle
+
+    if windower.ffxi.get_player().status == 1 then
+        set_equip = set_combine(sets.idle, sets.engage)
+        
+        if  mylib.is_in_adoulin(world.area) then
+            set_equip = set_combine(set_equip, sets.walk.adoulin)
+        end
+    end
+
+    if set_equip then
+        equip(set_equip)
+    end
+end
+
+function self_command(command)
+    if command == 'free' then
+        mode = nil
+        windower.add_to_chat('Mode is: '..mode)
+        setIdle()
+    elseif command == 'ho' then
+        mode = 'HORN'
+        windower.add_to_chat('Mode is: '..mode)
+        setIdle()
+    elseif command == 'ha' then
+        mode = 'HARP'
+        windower.add_to_chat('Mode is: '..mode)
+        setIdle()
+    -- elseif command == 'ma' then
+        -- mode = 'MARS'
+        -- windower.add_to_chat('Mode is: '..mode)
+        
+    elseif command == 'dummy' then
+        dummy = true
+        -- windower.add_to_chat('Dummy mode is ON')
+        
+    elseif command == 'hp' then
+        cast_init()
+        add_spell('so', '戦士達のピーアンVI')
+        add_spell('so', '戦士達のピーアンV')
+        add_spell('so', '戦士達のピーアンIV')
+        add_spell('so', '戦士達のピーアンIII')
+        if buffactive['クラリオンコール'] then
+            add_spell('so', '戦士達のピーアンII')
+        end
+        cast_all()
+    elseif command == 'aamm' then
+        cast_init()
+        add_spell('so', '栄典の戴冠マーチ')
+        add_spell('so', '猛者のメヌエットV')
+        add_spell('so', '猛者のメヌエットIV')
+        add_spell('so', '栄光の凱旋マーチ')
+        -- if buffactive['クラリオンコール'] then
+            -- add_spell('so', '猛者のメヌエットIII')
+        -- end
+        cast_all()
+    end
+end
---
+++ SCH.lua
@@ -0,0 +1,221 @@
+
+mylib = require('mylib')
+require('mylibs/caster')
+
+function get_sets()
+    set_language('japanese')
+    
+    sets.ws = {}
+
+    pdt = false
+    
+    sets.idle = {
+        head="ＡＢボネット+1",
+        body="ＡＢガウン+1",
+        hands="ＡＢブレーサー+1",
+        -- legs="ニャメフランチャ",
+        feet="ＡＢローファー+1",
+        right_ring="シュネデックリング",
+        back={ name="ルッフケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Mag.Atk.Bns."+10','Phys. dmg. taken-10%',}}
+    }
+    sets.pdt = {
+        head="ニャメヘルム",
+        body="ニャメメイル",
+        hands="ニャメガントレ",
+        legs="ニャメフランチャ",
+        feet="ニャメソルレット",
+    }
+    
+    sets.ja = {}
+    
+    sets.ma = {}
+    sets.ma.EnhancingDuration = {}
+    sets.ma.Stoneskin = set_combine(sets.ma.EnhancingDuration,{
+        waist="ジーゲルサッシュ",
+        neck="ノデンズゴルゲット",
+    })
+    sets.ma.Cure = {
+        neck="ノデンズゴルゲット",
+        feet="ヴァニヤクロッグ",
+        left_ear="メンデカントピアス",
+        right_ear="カラミタスピアス",
+        left_ring="ナジの包帯",
+    }
+
+    sets.buff = {
+        waist="エンブラサッシュ",
+    }
+    
+    sets.mb = {
+        ammo={ name="ガストリタスラム+1", augments={'Path: A',}},
+        neck={ name="アギュトストール+2", augments={'Path: A',}},
+        waist="オルペウスサッシュ",
+        left_ear={ name="アバテルピアス", augments={'System: 1 ID: 1676 Val: 0','Mag. Acc.+8',}},
+        right_ear="マリグナスピアス",
+        right_ring="メタモルリング+1",--{ name="メタモルリング+1", augments={'Path: A',}},
+        back={ name="ルッフケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Mag.Atk.Bns."+10','Phys. dmg. taken-10%',}},
+    }
+
+    sets.fc = {
+        ammo="インカントストーン",
+        head={ name="マーリンフード", augments={'Mag. Acc.+11 "Mag.Atk.Bns."+11','"Fast Cast"+7',}},
+        hands={ name="ＧＥゲージ+1", augments={'Phys. dmg. taken -4%','Magic dmg. taken -3%','Song spellcasting time -5%',}},
+        legs="ギーヴトラウザ",
+        feet={ name="マーリンクラッコー", augments={'"Fast Cast"+4','INT+4','Mag. Acc.+8','"Mag.Atk.Bns."+12',}},
+        waist="エンブラサッシュ",
+        left_ring="キシャールリング",
+        right_ring="プロリクスリング",
+        right_ear="マリグナスピアス",
+        left_ear="ロケイシャスピアス",--"エンチャンピアス+1",
+        back={ name="ルッフケープ", augments={'"Fast Cast"+10',}},
+    }
+
+    -- Common equipments
+    sets.walk = {
+        right_ring="シュネデックリング",
+    }
+    sets.walk.adoulin = set_combine(sets.walk, {
+        body="カウンセラーガーブ",
+    })
+    
+    -- {
+        -- main={name="ブンジロッド", augments={'Path: A',}},    --T3
+        -- sub="アムラピシールド",    --Omen Kei
+            -- ammo={ name="ガストリタスラム+1", augments={'Path: A',}},    --Unity Specter Worm
+            -- head={ name="ＰＤボード+3", augments={'Enh. "Altruism" and "Focalization"',}},    --AF2
+            -- body="ＡＢガウン+3",    --AF3
+            -- hands="ＡＢブレーサー+3",    --AF3
+        -- legs={ name="アグゥスロップス", augments={'Path: A',}},    --T3
+            -- feet="ＡＢローファー+3",    --AF3
+        -- neck={ name="アギュトストール+2", augments={'Path: A',}},    --AC
+            -- waist={ name="アキュイテベルト+1", augments={'Path: A',}},      --缺鱗
+        -- left_ear="王将の耳飾り",      --Omen Ou
+            -- right_ear="マリグナスピアス",
+        -- left_ring="フレキリング",     --Odin
+            -- right_ring={ name="メタモルリング+1", augments={'Path: A',}},      --缺鱗
+        -- back={ name="ルッフケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Mag.Atk.Bns."+10','Phys. dmg. taken-10%',}} --Ambuscade
+    -- }
+
+    -- FC = {
+        -- ammo="オンブルタスラム+1",  --Walk
+        -- head={ name="マーリンフード", augments={'"Mag.Atk.Bns."+3','"Fast Cast"+6','Mag. Acc.+11',}},       --DPoint
+        -- body="ピンガチュニック",       --Su3
+        -- hands={ name="マーリンダスタナ", augments={'Mag. Acc.+9','"Fast Cast"+7','"Mag.Atk.Bns."+15',}},       --DPoint
+        -- legs="ピンガズボン",          --Su3
+        -- feet={ name="マーリンクラッコー", augments={'Mag. Acc.+23 "Mag.Atk.Bns."+23','"Fast Cast"+5','Mag. Acc.+10',}},       --DPoint
+        -- neck="サンクトネックレス",       --DPoint 
+            -- waist="エンブラサッシュ",       --DPoint
+        -- left_ear="エンチャンピアス+1",  --Walk
+            -- right_ear="マリグナスピアス",
+        -- left_ring="キシャールリング",       --Omen GG
+        -- right_ring="プロリクスリング",      --VW
+        -- back={ name="ルッフケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Fast Cast"+10',}}
+    -- }
+
+    -- Magic = {    
+        -- sub="アムラピシールド",    --Omen Kei
+            -- ammo="ガストリタスラム",
+            -- head="ＡＢボネット+2",    --AF3
+            -- body="ＡＢガウン+3",    --AF3    --AF3
+            -- hands="ＡＢブレーサー+3",    --AF3
+        -- legs="アグゥスロップス",    --T3
+        -- feet="アグゥピガッシュ",    --T3
+        -- neck={ name="アギュトストール+2", augments={'Path: A',}},
+            -- waist="オルペウスサッシュ",
+        -- left_ear="王将の耳飾り",
+            -- right_ear="マリグナスピアス",
+            -- left_ring={ name="メタモルリング+1", augments={'Path: A',}},
+        -- right_ring="女王の指輪+1",
+        -- back={ name="ルッフケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Fast Cast"+10',}}
+    -- }
+
+
+
+
+    send_command('input /macro book 12; wait 2;input /lockstyleset 7')
+end
+
+function precast(spell)
+    local set_equip = nil
+
+    if spell.type == 'Trust' or string.find(spell.type, 'Magic') then
+        set_equip = sets.fc
+    end
+
+    if set_equip then
+        equip(set_equip)
+    end
+end
+
+function midcast(spell)
+    local set_equip = nil
+
+    if sets.ja[spell.english] then
+        set_equip = sets.ja[spell.english]
+    
+    elseif spell.action_type == 'Magic' then
+        if string.find(spell.english,'Cur') and spell.name ~= 'Cursna' then
+            set_equip = sets.ma.Cure
+            
+        elseif spell.skill=='強化魔法' then
+            set_equip = sets.buff
+            
+        elseif sets.ma[spell.english] then
+            set_equip = sets.ma[spell.english]
+            
+        elseif spell.skill=='精霊魔法' then
+            set_equip = sets.mb
+        end
+    end
+
+    if set_equip then
+        equip(set_equip)
+    end
+end
+
+function aftercast(spell)
+    setIdle()
+end
+
+function status_change(new,old)
+    setIdle()
+end
+
+function setIdle()
+    local set_equip = sets.idle
+
+    if windower.ffxi.get_player().status == 1 then
+        set_equip = set_combine(sets.idle, sets.engage)
+        
+        if  mylib.is_in_adoulin(world.area) then
+            set_equip = set_combine(set_equip, sets.walk.adoulin)
+        end
+    end
+
+    if set_equip then
+        equip(set_equip)
+    end
+end
+
+function self_command(command)
+    if command == 'pdt' then
+        pdt = not pdt
+        windower.add_to_chat('PDT- is: '..tostring(pdt))
+    elseif command == 's' then
+        windower.add_to_chat('==============================')
+        windower.add_to_chat('PDT- is: '..tostring(pdt))
+        
+    elseif command == 'suni' then
+        cast_init()
+        add_spell('ja', '女神降臨の章')
+        add_spell('ma', 'スニーク')
+        cast_all()
+    elseif command == 'regen' then
+        cast_init()
+        add_spell('ja', '女神降臨の章')
+        add_spell('ma', 'リジェネIV')
+        cast_all()
+    end
+    
+    setIdle()
+end
