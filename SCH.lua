8277e0c95e76740c253ebda84c60275728af992d
Cliff Chen <cfchen@futuredial.com>
2024-12-19 18:22:03 +0800

    Update equips for sch

--- SCH.lua
+++ SCH.lua
@@ -8,24 +8,33 @@ function get_sets()
     pdt = false
     
     sets.idle = {
-        head="ＡＢボネット+1",
-        body="ＡＢガウン+1",
-        hands="ＡＢブレーサー+1",
-        -- legs="ニャメフランチャ",
-        feet="ＡＢローファー+1",
-        neck={ name="アギュトストール+2", augments={'Path: A',}},
-        right_ring="シュネデックリング",
-        back={ name="ルッフケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Mag.Atk.Bns."+10','Phys. dmg. taken-10%',}}
-    }
-    sets.pdt = {
+        main="ムサ",
+        -- main="マリグナスポール",
+		ammo={ name="ガストリタスラム+1", augments={'Path: A',}},--'pdt',
+        sub="コーンスー",
         head="ニャメヘルム",
-        body="ニャメメイル",
+        body="ＡＢガウン+2",--"+3"
         hands="ニャメガントレ",
         legs="ニャメフランチャ",
         feet="ニャメソルレット",
+        waist="エンブラサッシュ",
+        neck={ name="アギュトストール+2", augments={'Path: A',}},
+        left_ear="ミミルピアス",
+        right_ear="サバントピアス",
+        left_ring="シーリチリング+1",--守
+        right_ring="シュネデックリング",
+        back={ name="ルッフケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Mag.Atk.Bns."+10','Phys. dmg. taken-10%',}}
     }
+	sets.idle.Sublimation = set_combine(sets.idle, {
+        head="ＡＣボード+2",
+        body="ＰＤガウン+1",--'ＰＤガウン+3',
+        right_ear="サバントピアス",
+	})
     
     sets.ja = {}
+    sets.ja['Tabula Rasa'] = {
+		legs="ペダゴギパンツ",
+	}
     
     sets.ma = {}
     sets.ma.EnhancingDuration = {}
@@ -35,30 +44,58 @@ function get_sets()
     })
     sets.ma.Cure = {
         neck="ノデンズゴルゲット",
+		body="ＡＢガウン+2",
+		legs="ギーヴトラウザ",
         feet="ヴァニヤクロッグ",
+		waist="オステリベルト+1",
         left_ear="メンデカントピアス",
         right_ear="カラミタスピアス",
         left_ring="ナジの包帯",
     }
 
     sets.buff = {
+		head={ name="テルキネキャップ", augments={'Mag. Evasion+25','"Conserve MP"+5','Enh. Mag. eff. dur. +10',}},
+		body={ name="テルキネシャジュブ", augments={'"Conserve MP"+5','Enh. Mag. eff. dur. +10',}},
+        hands="ＡＢブレーサー+2",--"ＡＢブレーサー+3",
+		legs={ name="テルキネブラコーニ", augments={'Mag. Evasion+25','"Conserve MP"+5','Enh. Mag. eff. dur. +10',}},
+		feet={ name="テルキネピガッシュ", augments={'Enh. Mag. eff. dur. +10',}},
+        neck="インカンタートルク",
         waist="エンブラサッシュ",
+        left_ear="ミミルピアス",
+    }
+    sets.ma.Regen = set_combine(sets.buff, {
+		head="ＡＢボネット+3",
+	})
+	sets.ma.storm = set_combine(sets.buff, {
+		feet="ペダゴギローファー",
+	})
+    
+    sets.lowsc = {
+        -- main="ハーミットワンド",
+        head={ name="カイロンハット", augments={'Spell interruption rate down -10%','VIT+10',}},
+        legs="クァーケンブレー",
+        body="ロゼトジャズラン+1",
+        hands={ name="カイロングローブ", augments={'Mag. Acc.+2','Spell interruption rate down -8%','CHR+4','"Mag.Atk.Bns."+4',}},
+        waist="エンパチコスロープ",
     }
     
     sets.mb = {
-        head="ニャメヘルム",
-        body="ニャメメイル",
-        hands="ニャメガントレ",
-        legs="ニャメフランチャ",
-        feet={ name="ニャメソルレット", augments={'Path: B',}},
-        left_ring="夢神の指輪",
+        head="ＡＢボネット+3",
+        body="ＡＢガウン+2",--"アグゥローブ",
+        hands="ＡＢブレーサー+2",--"アグゥゲージ",
+        legs="ＡＢパンツ+2",--"アグゥスロップス",
+        feet="ＡＢローファー+3",
         
         ammo={ name="ガストリタスラム+1", augments={'Path: A',}},
         neck={ name="アギュトストール+2", augments={'Path: A',}},
-        waist="オルペウスサッシュ",
-        left_ear={ name="アバテルピアス", augments={'System: 1 ID: 1676 Val: 0','Mag. Acc.+8',}},
+        waist="八輪の帯",
+        -- waist="オルペウスサッシュ",
+		left_ear="王将の耳飾り",
+        -- left_ear={ name="アバテルピアス", augments={'System: 1 ID: 1676 Val: 0','Mag. Acc.+8',}},
         right_ear="マリグナスピアス",
-        right_ring="メタモルリング+1",--{ name="メタモルリング+1", augments={'Path: A',}},
+        left_ring="フレキリング",
+        -- left_ring="夢神の指輪",
+        right_ring={ name="メタモルリング+1", augments={'Path: A',}},
         back={ name="ルッフケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Mag.Atk.Bns."+10','Phys. dmg. taken-10%',}},
     }
 
@@ -90,67 +127,11 @@ function get_sets()
     }
 
     -- Common equipments
-    sets.walk = {
-        right_ring="シュネデックリング",
-    }
+	sets.walk = {}
     sets.walk.adoulin = set_combine(sets.walk, {
         body="カウンセラーガーブ",
     })
     
-    -- {
-        -- main={name="ブンジロッド", augments={'Path: A',}},    --T3
-        -- sub="アムラピシールド",    --Omen Kei
-            -- ammo={ name="ガストリタスラム+1", augments={'Path: A',}},    --Unity Specter Worm
-            -- head={ name="ＰＤボード+3", augments={'Enh. "Altruism" and "Focalization"',}},    --AF2
-            -- body="ＡＢガウン+3",    --AF3
-            -- hands="ＡＢブレーサー+3",    --AF3
-        -- legs={ name="アグゥスロップス", augments={'Path: A',}},    --T3
-            -- feet="ＡＢローファー+3",    --AF3
-            -- neck={ name="アギュトストール+2", augments={'Path: A',}},    --AC
-            -- waist={ name="アキュイテベルト+1", augments={'Path: A',}},      --缺鱗
-        -- left_ear="王将の耳飾り",      --Omen Ou
-            -- right_ear="マリグナスピアス",
-        -- left_ring="フレキリング",     --Odin
-            -- right_ring={ name="メタモルリング+1", augments={'Path: A',}},      --缺鱗
-        -- back={ name="ルッフケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Mag.Atk.Bns."+10','Phys. dmg. taken-10%',}} --Ambuscade
-    -- }
-
-    -- FC = {
-        -- ammo="オンブルタスラム+1",  --Walk
-            -- head={ name="マーリンフード", augments={'"Mag.Atk.Bns."+3','"Fast Cast"+6','Mag. Acc.+11',}},       --DPoint
-        -- body="ピンガチュニック",       --Su3
-        -- hands={ name="マーリンダスタナ", augments={'Mag. Acc.+9','"Fast Cast"+7','"Mag.Atk.Bns."+15',}},       --DPoint
-        -- legs="ピンガズボン",          --Su3
-        -- feet={ name="マーリンクラッコー", augments={'Mag. Acc.+23 "Mag.Atk.Bns."+23','"Fast Cast"+5','Mag. Acc.+10',}},       --DPoint
-        -- neck="サンクトネックレス",       --DPoint 
-            -- waist="エンブラサッシュ",       --DPoint
-        -- left_ear="エンチャンピアス+1",  --Walk
-            -- right_ear="マリグナスピアス",
-            -- left_ring="キシャールリング",       --Omen GG
-            -- right_ring="プロリクスリング",      --VW
-            -- back={ name="ルッフケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Fast Cast"+10',}}
-    -- }
-
-    -- Magic = {    
-        -- sub="アムラピシールド",    --Omen Kei
-            -- ammo="ガストリタスラム",
-            -- head="ＡＢボネット+2",    --AF3
-            -- body="ＡＢガウン+3",    --AF3    --AF3
-            -- hands="ＡＢブレーサー+3",    --AF3
-        -- legs="アグゥスロップス",    --T3
-        -- feet="アグゥピガッシュ",    --T3
-            -- neck={ name="アギュトストール+2", augments={'Path: A',}},
-            -- waist="オルペウスサッシュ",
-        -- left_ear="王将の耳飾り",
-            -- right_ear="マリグナスピアス",
-            -- left_ring={ name="メタモルリング+1", augments={'Path: A',}},
-        -- right_ring="女王の指輪+1",
-            -- back={ name="ルッフケープ", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','INT+10','"Fast Cast"+10',}}
-    -- }
-
-
-
-
     send_command('input /macro book 12; wait 2;input /lockstyleset 7')
 end
 
@@ -159,10 +140,11 @@ function precast(spell)
 
     if spell.type == 'Trust' or string.find(spell.type, 'Magic') then
         set_equip = sets.fc
-        if spell.english == 'Adloquium' and (buffactive['白のグリモア']) then
-            cancel_spell()
-            send_command(windower.to_shift_jis('input /ja 女神降臨の章 <me>; wait 1; input /ma "'..spell.name..'" <me>'))
-        end
+
+        -- if spell.english == 'Adloquium' and (buffactive['白のグリモア']) then
+            -- cancel_spell()
+            -- send_command(windower.to_shift_jis('input /ja 女神降臨の章 <me>; wait 1; input /ma "'..spell.name..'" <me>'))
+        -- end
     end
 
     if set_equip then
@@ -179,12 +161,17 @@ function midcast(spell)
     elseif spell.action_type == 'Magic' then
         if string.find(spell.english,'Cur') and spell.name ~= 'Cursna' then
             set_equip = sets.ma.Cure
-            
-        elseif spell.skill=='強化魔法' then
-            set_equip = sets.buff
+        elseif string.find(spell.english,'Regen') then
+            set_equip = sets.ma.Regen
+			
+		elseif string.find(spell.english,'storm') then
+			set_equip = sets.ma.storm
             
         elseif sets.ma[spell.english] then
             set_equip = sets.ma[spell.english]
+            
+        elseif spell.skill=='強化魔法' then
+            set_equip = sets.buff
             
         elseif spell.skill=='精霊魔法' then
             set_equip = sets.mb
